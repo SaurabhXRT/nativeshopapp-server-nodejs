1a2804643568af8c04ae6eb9a867f2238568fb2c
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2023-12-30 10:15:20 +0530

    Add files via upload

---
+++ src/middleware/auth.js
@@ -0,0 +1,26 @@
+// middleware/auth.js
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const SecretKey ="2809a95eedde5863d8e8e3bea5205cd62d290b10a3769afee677b8754a4d05b7"
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, SecretKey); // Use your secret key
+    const user = await User.findOne({ _id: decoded.userId, 'tokens.token': token });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: 'Please authenticate' });
+  }
+};
+
+module.exports = authMiddleware;
