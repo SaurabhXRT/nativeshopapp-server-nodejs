09d0fe04a0c841040749801b4839a91bdcd0fc1e
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2023-12-30 10:08:38 +0530

    Update shopkeeper.js

---
+++ src/routes/shopkeeper.js
@@ -0,0 +1,31 @@
+// routes/shopkeeper.js
+const express = require('express');
+const router = express.Router();
+const Shopkeeper = require('../models/Shopkeeper');
+const authMiddleware = require('../middleware/auth'); // Add authentication middleware
+
+// Get shopkeepers for the authenticated user
+router.get('/', authMiddleware, async (req, res) => {
+  try {
+    const shopkeepers = await Shopkeeper.find({ createdBy: req.user._id });
+    res.json(shopkeepers);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Add a new shopkeeper for the authenticated user
+router.post('/', authMiddleware, async (req, res) => {
+  try {
+    const { name } = req.body;
+    const newShopkeeper = new Shopkeeper({ name, createdBy: req.user._id });
+    await newShopkeeper.save();
+    res.status(201).json(newShopkeeper);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
