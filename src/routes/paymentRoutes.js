056f8e23844680d18ba59727a1fc6500fe5424f2
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-01 16:38:10 +0530

    Create paymentRoutes.js

---
+++ src/routes/paymentRoutes.js
@@ -0,0 +1,27 @@
+// routes/paymentRoutes.js
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+const Payment = require('../models/Payment');
+
+// Save a new payment
+router.post('/:shopkeeperId/payments', authMiddleware, async (req, res) => {
+  try {
+    const { shopkeeperId } = req.params;
+    const { paidPrice } = req.body;
+
+    const payment = new Payment({
+      shopkeeper: shopkeeperId,
+      paidPrice,
+    });
+
+    await payment.save();
+
+    res.status(201).json(payment);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
