07197ce047b9c24f781130a6894fdb4c1b8a9c9f
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-02 23:00:46 +0530

    Update paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,11 +1,9 @@
-// routes/paymentRoutes.js
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
 const Payment = require('../models/Payment');
 const Shopkeeper = require('../models/ShopKeeper');
 
-// Save a new payment
 router.post('/:shopkeeperId/payments', authMiddleware, async (req, res) => {
   try {
     const { shopkeeperId } = req.params;
@@ -28,14 +26,10 @@ router.post('/:shopkeeperId/payments', authMiddleware, async (req, res) => {
 router.get('/:shopkeeperId/payments', async (req, res) => {
   try {
     const { shopkeeperId } = req.params;
-
-    // Check if the shopkeeper exists
     const shopkeeper = await Shopkeeper.findById(shopkeeperId);
     if (!shopkeeper) {
       return res.status(404).json({ error: 'Shopkeeper not found' });
     }
-
-    // Fetch payments for the shopkeeper
     const payments = await Payment.find({ shopkeeper: shopkeeperId });
     console.log(payments);
     res.status(200).json(payments);
