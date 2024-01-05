30aeec9b55c5df1b81150235be12399d4d97efc5
Saurabh kumar <111599314+SaurabhXRT@users.noreply.github.com>
2024-01-05 21:41:21 +0530

    Update shopkeeper.js

--- src/routes/shopkeeper.js
+++ src/routes/shopkeeper.js
@@ -43,11 +43,26 @@ router.delete('/:id', authMiddleware, async (req, res) => {
   }
 });
 
+router.get('/:id', authMiddleware, async (req, res) => {
+  try {
+    const shopkeeper = await Shopkeeper.find(req.params.id);
+
+    if (!shopkeeper) {
+      return res.status(404).json({ error: 'Shopkeeper not found' });
+    }
+
+    res.status(200).json(shopkeeper);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 router.get('/:shopkeeperId/items', authMiddleware, async (req, res) => {
   try {
-    const shopkeepername = await Shopkeeper.find(req.params.id);
+    //const shopkeepername = await Shopkeeper.find(req.params.id);
     const items = await ListItem.find({ shopkeeper: req.params.shopkeeperId }).sort({ createdAt: -1 });
-    res.status(200).json({items, shopkeepername});
+    res.status(200).json(items);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
